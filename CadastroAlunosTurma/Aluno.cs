30895d029be417b1b4187f418f2663e465109ed9
Leandro Andrade <leandrade666@gmail.com>
2024-04-01 13:41:50 -0300

    Criada a classe aluno com suas propriedades e criada a classe estática CadastroDeAlunos com o métodos estáticos, adcionarAluno,BuscarAlunoPorIndice e ExibirTodosOsAlunos

---
+++ CadastroAlunosTurma/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAlunosTurma
+{
+    class Aluno
+    {
+        public int Indice { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+
+    }
+}
---
+++ CadastroAlunosTurma/CadastroDeAlunos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAlunosTurma
+{
+   public static class CadastroDeAlunos
+    {
+        private static Aluno[] _alunos = new Aluno[100];
+        private static int _proximoIndice = 0;
+
+        public static void AdicionarAluno(string nome, string turma)
+        {
+            if(_proximoIndice < _alunos.Length)
+            {
+
+                _alunos[_proximoIndice] = new Aluno()
+                {
+                    Indice = _proximoIndice,
+                    Id = _proximoIndice,
+                    Nome = nome,
+                    Turma = turma,
+
+                };
+                _proximoIndice++;
+            }
+
+        }
+
+        public static Aluno BuscarAlunoPorIndice(int indice)
+        {
+            if (indice >= 0 && indice < _alunos.Length)
+            {
+                return _alunos[indice];
+            }
+            return null;
+        }
+        public static void ExibirTodosOsAlunos()
+        {
+            foreach(Aluno Aluno in _alunos)
+            {
+                if(Aluno!=null)
+                {
+                    Console.WriteLine($"\nÍndice: {Aluno.Indice}");
+                    Console.WriteLine($"\nId: {Aluno.Id}");
+                    Console.WriteLine($"\nNome: {Aluno.Nome}");
+                    Console.WriteLine($"\nTurma: {Aluno.Turma}");
+                }
+            }
+
+        }
+
+    }
+}
+
